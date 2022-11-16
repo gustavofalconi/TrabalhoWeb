ec2704a2a4fe08ac01bb1ee3e82de07f619c8c6b
Gustavo Falconi <91212083+gustavofalconi@users.noreply.github.com>
2022-11-16 08:52:44 -0300

    Add files via upload

---
+++ Projeto - ONU/js/animacoes.js
@@ -0,0 +1,14 @@
+$('.graficos').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$('.salas').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+});
